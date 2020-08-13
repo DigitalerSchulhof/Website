148e84d7addcdde436ac3e199371a82dcbe8a848
jeengbe <jesper.engberg@gmx.at>
2020-08-13 19:48:57 +0200

    [Fix] Besser Pfad interpretieren - [Neu] Brotkrumen

--- seiten/website.php
+++ seiten/website.php
@@ -32,6 +32,9 @@
   Wird im Nachhinein eine Sprache mit einem Kürzel angelegt, welches schon als direktes Unterverzeichnis (in der Standardsprache) von / verwendet wird, wird gewarnt, und dringend geraten, diese Seite umzubennen, bzw. das Kürzel zu wechseln
  */
 
+// Global machen
+global $versionen, $modi, $startseite, $standardversion, $standardmodus, $DSH_SPRACHE, $DSH_SEITENVERSION, $DSH_SEITENMODUS, $DSH_SEITENPFAD;
+
 $DSH_STANDARDSPRACHE = "DE";
 
 $DSH_SPRACHEN = array(
@@ -67,50 +70,128 @@ $startseite = array(
 );
 
 
-$url = $DSH_URL;
+$WEBSITE_URL = [];
 
 $fehler = false;
 
-switch(count($url)) {
-  case 1:
-    $url = array_merge($url, [$DSH_STANDARDSPRACHE, $versionen[$DSH_STANDARDSPRACHE][1], $modi[$DSH_STANDARDSPRACHE][1], $startseite[$DSH_STANDARDSPRACHE]]);
-    break;
-  case 2:
-    if(!in_array($url[1], array_keys($DSH_SPRACHEN))) {
-      $fehler = true;
+$standardmodus = 0;
+$standardversion = 1;
+
+// Website/Sprache/Version/Modus/Seiten..
+
+$url = $DSH_URL;
+
+if(count($url) > 1) {
+  if(in_array($url[1], array_keys($DSH_SPRACHEN))) {
+    // Sprache gegeben
+    $WEBSITE_URL[0] = $url[1];
+    if(count($url) > 2) {
+      if(in_array($url[2], $versionen[$WEBSITE_URL[0]])) {
+        // Version gegeben
+        $WEBSITE_URL[1] = $url[2];
+        if(count($url) > 3) {
+          if(in_array($url[3], $modi[$WEBSITE_URL[0]])) {
+            // Modus gegeben
+            $WEBSITE_URL[2] = $url[3];
+            if(count($url) > 4) {
+              // Seite gegeben
+
+              // Sprache, Version, Modus, Seite
+              array_shift($url);
+              array_shift($url);
+              array_shift($url);
+              array_shift($url);
+              $WEBSITE_URL = array_merge($WEBSITE_URL, $url);
+            } else {
+              // Sprache, Version, Modus, keine Seite
+              $WEBSITE_URL = array_merge($WEBSITE_URL, [$startseite[$WEBSITE_URL[0]]]);
+            }
+          } else {
+            // Sprache, Version, Seite
+            array_shift($url);
+            array_shift($url);
+            array_shift($url);
+            $WEBSITE_URL = array_merge($WEBSITE_URL, [$modi[$WEBSITE_URL[0]][$standardmodus]], $url);
+          }
+        } else {
+          // Sprache, Version, keine Seite
+          $WEBSITE_URL = array_merge($WEBSITE_URL, [$modi[$WEBSITE_URL[0]][$standardmodus]], [$startseite[$WEBSITE_URL[0]]]);
+        }
+      } else {
+        // Sprache, Seite
+        array_shift($url);
+        array_shift($url);
+        $WEBSITE_URL = array_merge($WEBSITE_URL, [$versionen[$WEBSITE_URL[0]][$standardversion], $modi[$WEBSITE_URL[0]][$standardmodus]], $url);
+      }
     } else {
-      $url = array_merge($url, [$versionen[$url[1]][1], $modi[$url[1]][0], $startseite[$url[1]]]);
+      // Nur Sprache, keine Seite
+      $WEBSITE_URL = array_merge($WEBSITE_URL, [$versionen[$WEBSITE_URL[0]][$standardversion], $modi[$WEBSITE_URL[0]][$standardmodus]], [$startseite[$WEBSITE_URL[0]]]);
     }
-    break;
-  case 3:
-    if(!in_array($url[1], array_keys($DSH_SPRACHEN)) || !in_array($url[2], $modi[$url[1]])) {
-      $fehler = true;
-    } else {
-      $url = array_merge($url, [$modi[$url[1]][0], $startseite[$url[1]]]);
-    }
-    break;
-  case 3:
-    if(!in_array($url[1], array_keys($DSH_SPRACHEN)) || !in_array($url[2], $versionen[$url[1]]) || !in_array($url[3], $modi[$url[1]])) {
-      $fehler = true;
-    } else {
-      $url = array_merge($url, [$startseite[$url[1]]]);
-    }
-    break;
-  default:
-    if(!in_array($url[1], array_keys($DSH_SPRACHEN)) || !in_array($url[2], $versionen[$url[1]]) || !in_array($url[3], $modi[$url[1]])) {
-      $fehler = true;
-    }
-    break;
+  } else {
+    // Seite
+    $WEBSITE_URL = array_merge($WEBSITE_URL, [$DSH_STANDARDSPRACHE, $versionen[$DSH_STANDARDSPRACHE][$standardversion], $modi[$DSH_STANDARDSPRACHE][$standardmodus]], $url);
+  }
+} else {
+  // keine Seite
+  $WEBSITE_URL = array_merge($WEBSITE_URL, [$DSH_STANDARDSPRACHE, $versionen[$DSH_STANDARDSPRACHE][$standardversion], $modi[$DSH_STANDARDSPRACHE][$standardmodus]], [$startseite[$DSH_STANDARDSPRACHE]]);
 }
 
-// Ab hier ist $url eine gültige Seite
+// Ab hier ist $WEBSITE_URL eine gültige Seite, OHNE Website/ vorne dran
 
-if($fehler) {
-  Anfrage::seiteAus($fehlerSeiten[$url[1]]);
+$DSH_SPRACHE        = $WEBSITE_URL[0];                                          // Sprachkürzel
+$DSH_SEITENVERSION  = array_search($WEBSITE_URL[1], $versionen[$DSH_SPRACHE]);  // 0 => Alt     1 => Aktuell  2 => Neu
+$DSH_SEITENMODUS    = array_search($WEBSITE_URL[2], $modi[$DSH_SPRACHE]);       // 0 => Sehen   1 => Bearbeiten
+$url = $WEBSITE_URL;
+$DSH_SEITENPFAD     = array_splice($url, 3);
+
+// Website/Sprache/Version/Modus/Seiten..
+
+class Seite extends Kern\Seite {
+  public function __construct($titel) {
+    parent::__construct($titel, false, true);
+  }
+
+  public function __toString() : string {
+    global $WEBSITE_URL;
+    $code = "";
+    if ($this->aktionszeile) {
+      global $versionen, $modi, $startseite, $standardversion, $standardmodus, $DSH_SPRACHE, $DSH_SEITENVERSION, $DSH_SEITENMODUS, $DSH_SEITENPFAD;
+      $pfad = array(
+        "Website/$DSH_SPRACHE/{$versionen[$DSH_SPRACHE][$standardversion]}/{$modi[$DSH_SPRACHE][$standardmodus]}/{$startseite[$DSH_SPRACHE]}" => "Website",
+      );
+      $pf = "";
+      foreach($DSH_SEITENPFAD as $i => $p) {
+        if($i === count($DSH_SEITENPFAD)-1) {
+          // Letzte Seite
+
+          $extra = [];
+          if($DSH_SEITENVERSION !== $standardversion) {
+            $extra[] = $versionen[$DSH_SPRACHE][$DSH_SEITENVERSION];
+          }
+          if($DSH_SEITENMODUS !== $standardmodus) {
+            $extra[] = $modi[$DSH_SPRACHE][$DSH_SEITENMODUS];
+          }
+          if(count($extra) > 0) {
+            $p .= " (".join(", ", $extra).")";
+          }
+
+          $pfad = array_merge($pfad, array("Website/$DSH_SPRACHE/{$versionen[$DSH_SPRACHE][$DSH_SEITENVERSION]}/{$modi[$DSH_SPRACHE][$DSH_SEITENMODUS]}/$pf$p" => "$p"));
+        } else {
+          $pfad = array_merge($pfad, array("Website/$DSH_SPRACHE/{$versionen[$DSH_SPRACHE][$standardversion]}/{$modi[$DSH_SPRACHE][$standardmodus]}/$pf$p" => $p));
+          $pf .= "$p/";
+        }
+      }
+      $code .= (new Kern\Aktionszeile())->setBrotkrumenPfad($pfad);
+    }
+    foreach ($this->zeilen as $z) {
+      $code .= $z;
+    }
+    return $code.$this->codedanach;
+  }
 }
 
-$SEITE = new Kern\Seite("Website", false);
+$SEITE = new Seite("Website");
 
-$SEITE[] = UI\Zeile::standard(new UI\Meldung("Gültige URL", "Die URL wurde folgendermaßen interpretiert: <b>".join("/", $url)."</b>", "Erfolg"));
+$SEITE[] = UI\Zeile::standard(new UI\Meldung("Gültige URL", "Die URL wurde folgendermaßen interpretiert: <b>".join("/", $WEBSITE_URL)."</b>", "Erfolg"));
 
 ?>
